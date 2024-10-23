b610e8406162c58015d426bb88ad8da6e7038fbe
Po <921006eason@gmail.com>
2024-10-24 00:25:23 +0800

    介面修改

--- app.py
+++ app.py
@@ -8,22 +8,35 @@ import tkinter as tk
 import customtkinter as ctk
 from PIL import Image, ImageTk
 from neo4j import GraphDatabase
+from dotenv import load_dotenv
 # Langchain 相關導入
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import Neo4jVector
 from langchain.chains import GraphCypherQAChain
 from langchain_community.graphs import Neo4jGraph
 from langchain_core.prompts import PromptTemplate
 # ========== 初始化設置 ==========
+# 獲取當前腳本的目錄
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 打印當前工作目錄
+print("當前工作目錄:", current_dir)
+
+# 嘗試手動載入 .env 文件
+env_path = os.path.join(current_dir, '.env')
+print("嘗試從以下位置載入 .env 文件:", env_path)
+load_dotenv(env_path)
 
 # 配置
-GOOGLE_API_KEY = "AIzaSyByxnWsalrjFdslxrJ6GHsIby_Sl30IXS8"
-NEO4J_URL = "bolt://localhost:7687"
-NEO4J_USERNAME = "neo4j"
-NEO4J_PASSWORD = "123456789"
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+NEO4J_URL =  os.getenv("NEO4J_URL")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 # 設置 Google API and Model
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
-gemini_model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.5)
+Gpt_model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 # Neo4j 連接
 graph = Neo4jGraph(
@@ -55,7 +68,7 @@ logo_label = ctk.CTkLabel(
     font=ctk.CTkFont(size=30, weight="bold")
 )
 logo_label.pack(padx=20, pady=(40, 40))
-image = Image.open("D:/ai/mcu_113_project/TKinter/bot.png")
+image = Image.open("TKinter/bot.png")
 chat_image = ctk.CTkImage(light_image=image, dark_image=image, size=(30, 30))
 logo_label.configure(image=chat_image, compound="right")
 
@@ -187,20 +200,20 @@ def process_url():
         input_frame.pack(fill="x", padx=20, pady=(0, 20))      
     
 # ========== 按鈕創建 ==========
-create_button("PDF  ", "D:/ai/mcu_113_project/TKinter/pdf.png", show_PDF)
-create_button("Audio", "D:/ai/mcu_113_project/TKinter/mp3.png", show_MP3)
-create_button("Web  ", "D:/ai/mcu_113_project/TKinter/web.png", show_url_input)
-create_button("Excel", "D:/ai/mcu_113_project/TKinter/excel.png", show_Excel)
+create_button("PDF  ", "TKinter/pdf.png", show_PDF)
+create_button("Audio", "TKinter/mp3.png", show_MP3)
+create_button("Web  ", "TKinter/web.png", show_url_input)
+create_button("Excel", "TKinter/excel.png", show_Excel)
 # ========== 頂部標題 ==========
 ai_chat = ctk.CTkLabel(right_frame, 
                        text="AI Chat", 
                        font=ctk.CTkFont(size=20,weight="bold"))
 ai_chat.pack(padx=40, pady=(20, 10))
 # 載入圖片
-image = Image.open("D:/ai/mcu_113_project/TKinter/ai.png")
+image = Image.open("TKinter/ai.png")
 chat_image = ctk.CTkImage(light_image=image, dark_image=image, size=(20, 20))
 ai_chat.configure(image=chat_image, compound="left")
-image_load("D:/ai/mcu_113_project/TKinter/ai.png",ai_chat,20,20)
+image_load("TKinter/ai.png",ai_chat,20,20)
 # ========== 右邊聊天框 ==========
 
 #聊天框創建
@@ -228,17 +241,58 @@ chat_input = ctk.CTkEntry(input_frame,
 chat_input.pack(side="left", expand=True, fill="x")
 
 # 創建發送按鈕
-send_image = Image.open("D:/ai/mcu_113_project/TKinter/upload.png")
+send_image = Image.open("TKinter/upload.png")
 send_icon = ctk.CTkImage(light_image=send_image, dark_image=send_image, size=(30, 30))
 
-#cypher查詢
-cypher_chain = GraphCypherQAChain.from_llm(
-    cypher_llm=gemini_model,
-    qa_llm=gemini_model,
+vector_index = Neo4jVector.from_existing_graph(
+    embeddings,
+    url=NEO4J_URL,
+    username=NEO4J_USERNAME,
+    password=NEO4J_PASSWORD,
+    node_label="Document",
+    index_name="entity_embedding_index",
+    text_node_properties=["text"],
+    embedding_node_property="embedding"
+)
+
+# 創建 GraphCypherQAChain
+def genarate_answer(question):
+    cypher_prompt =PromptTemplate.from_template("""
+    你是一個 Neo4j專家。根據以下資訊生成一個 Cypher 查詢：
+    其中where請依照用戶提到的實體來撰寫，你的cypher查詢必須遵守範例中格式，但你可以根據用戶提問來調整cypher查詢。
+    當用戶提到某些詞語時，請提取它並作為搜尋實體的依據。
+    例如:
+    MATCH (e1:Entity)-[r:Relationship]->(e2:Entity)
+    WHERE e1.name = "name" 
+    RETURN e1, r.type,e2
+    請務必使用用戶提到的實體生成一個準確的 Cypher 查詢來回答問題。只返回 Cypher 查詢得到的所有數據。
+    用戶問題：{question}
+                                                                                                                                    
+
+    """                                               
+    )
+    LLM_qa_prompt = """
+    你是一個專家。專精於尋找資料庫中的資訊並結合自身知識來回答問題。
+    將得到的實體關係模仿人類聊天的方式回答，請記得，你的回答必須是基於neo4j的實體關係，而且必須盡可能列出相關的實體，並利用這些實體組成句子。
+    如果無法利用實體關係回答或訊息不足，則利用自身知識回答。
+    如果還是不行，請說你不知道。
+
+    Cypher 查詢結果：{context}
+    用戶問題：{question}
+    """
+    prompt_qa = PromptTemplate(template=LLM_qa_prompt, input_variables=["question","context"])
+
+    cypher_chain = GraphCypherQAChain.from_llm(
+    llm=Gpt_model,
     graph=graph,
     verbose=True,
+    cypher_prompt=cypher_prompt,
+    qa_prompt=prompt_qa,
+    validate_cypher=True,
     allow_dangerous_requests=True
-)
+    )
+    response = cypher_chain.invoke({"query":question})
+    return response
 
 def send_message(event=None):
     """輸入訊息處理"""
@@ -251,12 +305,13 @@ def send_message(event=None):
         chat_display._textbox.tag_configure("user", justify="right", lmargin2=50)
         chat_display.insert("end", f" {message}\n", "user")
         chat_display.insert("end", "\n")  # 添加額外的換行以分隔消息
-        cypher_response = cypher_chain.invoke({"query": message})
-        final_response = cypher_response.get('result', '抱歉，我無法回答這個問題。')
+        response = genarate_answer(message)
+        print(response)
+        ai_response = response.get('result', '抱歉，我無法回答這個問題。')
         
         # 插入AI回應，並應用左對齊標籤
         chat_display._textbox.tag_configure("ai", justify="left", rmargin=50)
-        chat_display.insert("end", f"AI: {final_response}\n", "ai")
+        chat_display.insert("end", f"AI: {ai_response}\n", "ai")
         chat_display.insert("end", "\n")  
         chat_display.configure(state="disabled")
        
